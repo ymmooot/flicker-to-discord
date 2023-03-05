a01793dc93662b25b631d4da44b734a3052c8789
ymmooot <ryota.yama.9+github@gmail.com>
2023-03-05 20:43:48 +0900

    Add user info

--- index.ts
+++ index.ts
@@ -9,6 +9,13 @@ type Env = {
   discordWebhookUrl: string;
 };
 
+type FlickrPayload = {
+  id: string;
+  userName: string;
+  images: string[];
+  iconUrl: string;
+};
+
 const loadEnv = (): Env => {
   const flickrIDs = env.require("FLICKR_IDS");
   const discord = env.require("DISCORD_WEBHOOK_URL");
@@ -20,51 +27,69 @@ const loadEnv = (): Env => {
   };
 };
 
-const fetchRSS = async (url: string): Promise<[string, string[]]> => {
+const fetchRSS = async (url: string): Promise<FlickrPayload> => {
   const response = await fetch(url);
   const xml = await response.text();
   const feed = await rss.parseFeed(xml);
   const targetEntries = feed.entries.filter((e) =>
     e.published!.getTime() > Date.now() - fetchInterval
   );
+  const userName = feed.title.value?.match(/(from (?<user>.*))/)?.groups?.user!;
   const images = targetEntries.flatMap((e) =>
     e["media:content"]!.map((c) => c.url!)
   );
-  return [feed.id, images];
+  return {
+    id: feed.id,
+    userName,
+    images,
+    iconUrl: feed.image!.url!,
+  };
 };
 
-const sendToDiscord = async (id: string, url: string, imageUrls: string[]) => {
-  if (imageUrls.length === 0) {
+const sendToDiscord = async (payload: FlickrPayload, webhookUrl: string) => {
+  if (payload.images.length === 0) {
     return;
   }
 
   const batchSize = 4; // four images per batch
-  const batches = Math.ceil(imageUrls.length / batchSize);
+  const batches = Math.ceil(payload.images.length / batchSize);
 
   for (let i = 0; i < batches; i++) {
     const batchStart = i * batchSize;
     const batchEnd = batchStart + batchSize;
-    const batch = imageUrls.slice(batchStart, batchEnd);
+    const batch = payload.images.slice(batchStart, batchEnd);
 
     const embeds = batch.map((url) => ({
-      url: id,
+      url: payload.id,
       image: {
         url,
       },
+      author: i !== 0 ? undefined : {
+        name: payload.userName,
+        url: payload.id,
+        icon_url: payload.iconUrl,
+      },
     }));
 
-    await fetch(url, {
+    const json = {
+      username: `Photos by ${payload.userName}`,
+      avatar_url:
+        "https://www.flickrhelp.com/hc/article_attachments/4419907666708/unnamed.png",
+      embeds,
+    };
+
+    await fetch(webhookUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ embeds }),
+      body: JSON.stringify(json),
     });
   }
 };
 
 const e = loadEnv();
 for (const url of e.flickerRSSUrls) {
-  const [id, images] = await fetchRSS(url);
-  await sendToDiscord(id, e.discordWebhookUrl, images);
+  const payload = await fetchRSS(url);
+  await sendToDiscord(payload, e.discordWebhookUrl);
 }
